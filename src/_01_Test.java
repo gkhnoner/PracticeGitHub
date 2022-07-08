d87e55fc54c1eb8cbdc659e3f0c63dc17b5d8b8f
gkhnoner <mgokhanoner@gmail.com>
2022-07-07 21:48:22 -0400

    Added numbers array to 01test class

---
+++ src/_01_Test.java
@@ -0,0 +1,14 @@
+public class _01_Test {
+
+    public static void main(String[] args) {
+
+        int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        for (int i=0; i<numbers.length; i++) {
+            System.out.println(numbers[i] + " ");
+        }
+
+
+    }
+
+}
